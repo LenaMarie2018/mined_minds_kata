a67e91c52c13c38804bd5614b76d07879d582ea5
LenaMarie2018 <lenashandor@gmail.com>
2018-02-19 10:50:36 -0500

    div by 3 mined div by 5 minds div by 3 and 5 mined minds

--- MinedMinds_kata.rb
+++ MinedMinds_kata.rb
@@ -1,11 +1,15 @@
 num=1
 
 100.times do
-	if num % 3==0 
+	if num % 3==0 && num % 5==0
+		puts "Mined Minds"
+
+	elsif num % 3==0 
 		puts "Mined"
 	
 	elsif num % 5==0
 		puts "Minds"
+		
 	else 
  		puts (num)
  	end
