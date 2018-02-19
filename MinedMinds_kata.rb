2c95296955b5bab704d5790fb09ba0a9c6e4ab15
LenaMarie2018 <lenashandor@gmail.com>
2018-02-19 10:11:43 -0500

    replacing all numbers div by 3 with minded

--- MinedMinds_kata.rb
+++ MinedMinds_kata.rb
@@ -1,10 +1,16 @@
 num=1
 
-100.times do
-	if (num % 5)===0
-	puts "minds"
-else 
-	puts (num)
-end 
-	num +=1
-		end
+99.times do
+num=num+1
+	if num % 3==0
+		puts "Mined"
+	else 
+		puts (num)
+	end
+
+	# if num % 5==0
+	# 	puts "Minds"
+	# else 
+ # 		puts (num)
+ # 	end
+ end 
